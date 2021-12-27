b485127da111417fc59f50ff6ea3621d217095b0
Silvanis <silvanis.silva@outlook.com>
2021-12-27 16:32:20 -0300

    My first commit in Java

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,38 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência as bibliotecas
+
+
+// 3 - classe
+public class Medidas {
+    // 3.1 - atributos - caracteristicas
+
+    // 3.2 - métodos e funções
+    public static void main(String[] args){
+
+
+
+    }
+
+    public void calcularAreaModoExtenso() {
+        // Calculando tamanho de uma área, exemplo: tamanho de uma sala ou de tapete.
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5;
+        comprimento = 6;
+
+        resultado = largura * comprimento;
+        System.out.println("Para largura de " + largura + " E comprimento de "+ comprimento
+                + " O resultado será: " + resultado + " m²");
+
+    }
+
+    public void calcularAreaModoCompacto() {
+        int largura = 4;
+        int comprimento = 3;
+        System.out.println("A área é de: " + largura*comprimento);
+    }
+}
