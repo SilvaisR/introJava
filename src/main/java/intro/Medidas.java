4ea2ab74417e4a8b932f541acfe2b9d3f2f27f18
Silvanis <silvanis.silva@outlook.com>
2021-12-28 11:50:29 -0300

    Aprendendo a usar if e switchs em Java

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -3,43 +3,102 @@ package intro;
 
 // 2 - Referência as bibliotecas
 
+import java.util.Scanner;
+
+
 
 // 3 - classe
 public class Medidas {
     // 3.1 - atributos - caracteristicas
 
     // 3.2 - métodos e funções
-    public static void main(String[] args){
-        System.out.println("Bom dia!!!");
-        System.out.println("Vamos calcular áreas! ");
 
-        calcularAreaModoCurto();
-        calcularAreaModoExtenso();
+
+    public static void main(String[] args) {
+
+        // Usando o método scanner para poder ler um valor do usuário
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("De 1 à 12, qual é sua escolha?   ");
+
+        String opcao = scanner.next();
+
+        switch (opcao){
+            case "1":
+                System.out.println("Janeiro");
+                break;
+            case "2":
+                System.out.println("Fevereiro");
+                break;
+            case "3":
+                System.out.println("Março");
+                break;
+            case "4":
+                System.out.println("Maio");
+                break;
+            case "5":
+                System.out.println("Abril");
+                break;
+            case "6":
+                System.out.println("Junho");
+                break;
+            case "7":
+                System.out.println("Julho");
+                break;
+            case "8":
+                System.out.println("Agosto");
+                break;
+            case "9":
+                System.out.println("Setembro");
+                break;
+            case "10":
+                System.out.println("Outubro");
+                break;
+            case "11":
+                System.out.println("Novembro");
+                break;
+            case "12":
+                System.out.println("Dezembro");
+                break;
+            default:
+                System.out.println("Número inválido.");
+                break;
+        }
 
     }
 
-    public static void calcularAreaModoExtenso() {
-        // Calculando tamanho de uma área, exemplo: tamanho de uma sala ou de tapete.
-        int largura;
-        int comprimento;
-        int resultado;
 
-        largura = 5;
-        comprimento = 6;
+//    public static void ifSimples() {
+//            String msg = "curto";
+//
+//            if (msg == "curto") {
+//                calcularAreaModoCurto();
+//            } else {
+//                calcularAreaModoExtenso();
+//            }
+//        }
+//
+//    public static void calcularAreaModoExtenso() {
+//        // Calculando tamanho de uma área, exemplo: tamanho de uma sala ou de tapete.
+//        int largura;
+//        int comprimento;
+//        int resultado;
+//
+//        largura = 5;
+//        comprimento = 6;
+//
+//        System.out.println("Cálculo de área modo extenso: ");
+//
+//        resultado = largura * comprimento;
+//        System.out.println("Para largura de " + largura + " E comprimento de "+ comprimento
+//                + " O resultado será: " + resultado + " m²");
+//
+//    }
 
-        System.out.println("Cálculo de área modo extenso: ");
-
-        resultado = largura * comprimento;
-        System.out.println("Para largura de " + largura + " E comprimento de "+ comprimento
-                + " O resultado será: " + resultado + " m²");
-
-    }
-
-    public static void calcularAreaModoCurto() {
-
-        System.out.println("Cálculo de área modo curto: ");
-        int largura = 4;
-        int comprimento = 3;
-        System.out.println("A área é de: " + largura*comprimento);
-    }
+//    public static void calcularAreaModoCurto() {
+//
+//        System.out.println("Cálculo de área modo curto: ");
+//        int largura = 4;
+//        int comprimento = 3;
+//        System.out.println("A área é de: " + largura*comprimento);
+//    }
 }
