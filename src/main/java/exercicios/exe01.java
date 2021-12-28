43e4ff0d45f43400ee4e49f2e0c9b9dc9161fccd
Silvanis <silvanis.silva@outlook.com>
2021-12-28 14:20:25 -0300

    Começando a série de exercícios do extremo básico.

---
+++ src/main/java/exercicios/exe01.java
@@ -0,0 +1,10 @@
+package exercicios;
+// Crie um programa que escreva "Olá mundo!" na tela.
+
+public class exe01 {
+
+    public static void main(String[] args){
+        System.out.println("Olá mundo!");
+    }
+
+}
