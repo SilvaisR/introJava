531529ca9c3c106729a53af031ede853f00a8b98
Silvanis <silvanis.silva@outlook.com>
2021-12-28 08:43:41 -0300

    Aprendendo a chamar funções em Java.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -11,12 +11,14 @@ public class Medidas {
     // 3.2 - métodos e funções
     public static void main(String[] args){
         System.out.println("Bom dia!!!");
-        System.out.println("Calculo de área.");
+        System.out.println("Vamos calcular áreas! ");
 
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
 
     }
 
-    public void calcularAreaModoExtenso() {
+    public static void calcularAreaModoExtenso() {
         // Calculando tamanho de uma área, exemplo: tamanho de uma sala ou de tapete.
         int largura;
         int comprimento;
@@ -25,13 +27,17 @@ public class Medidas {
         largura = 5;
         comprimento = 6;
 
+        System.out.println("Cálculo de área modo extenso: ");
+
         resultado = largura * comprimento;
         System.out.println("Para largura de " + largura + " E comprimento de "+ comprimento
                 + " O resultado será: " + resultado + " m²");
 
     }
 
-    public void calcularAreaModoCompacto() {
+    public static void calcularAreaModoCurto() {
+
+        System.out.println("Cálculo de área modo curto: ");
         int largura = 4;
         int comprimento = 3;
         System.out.println("A área é de: " + largura*comprimento);
