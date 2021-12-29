7a1d90cc65681427f905b779767f3882ba050f0d
Silvanis <silvanis.silva@outlook.com>
2021-12-29 09:02:52 -0300

    Preciso aprender a usar exponenciação para poder terminar o exercício 6.

---
+++ src/main/java/exercicios/exe06.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+// Crie um algorítmo que leia um número do teclado e mostre na tela, o seu dobro, triplo e a raiz quadrada.
+
+import java.util.Scanner;
+
+public class exe06 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = Integer.parseInt(scanner.next());
+        System.out.println();
+        int dobro = num * 2;
+        int triplo = num * 3;
+        int raiz = num ** (1/2);
+        System.out.println("O número digitado foi: " + num + "\n" +
+                "O seu dobro é: " + dobro + "\n" +
+                "Seu triplo é: " + triplo + "\n" +
+                "Sua raiz quadrada é: " + raiz);
+    }
+
+}
